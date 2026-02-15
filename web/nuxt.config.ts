55497c24af9d51e7685fb4730eca7225557b85eb
Roberto Martins <b0bd3v@gmail.com>
2026-02-15 13:58:45 -0300

    chore(vuetify): Install and initial setup

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,5 +1,23 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
 export default defineNuxtConfig({
+  css: ['vuetify/styles', '@mdi/font/css/materialdesignicons.min.css'],
+  build: {
+    transpile: ['vuetify'],
+  },
+
+  vite: {
+    plugins: [
+      // @ts-ignore
+      vuetify({ autoImport: true })
+    ],
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
 })
